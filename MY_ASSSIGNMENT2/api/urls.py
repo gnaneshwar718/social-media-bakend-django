ae982ea21923659ef2d633bcfab9d8fff8c75f0b
gnaneshwar.p <gnaneshwar718@gmail.com>
2024-05-03 12:53:10 +0530

    1

--- MY_ASSSIGNMENT2/api/urls.py
+++ MY_ASSSIGNMENT2/api/urls.py
@@ -9,6 +9,8 @@ from .views import (
 
 urlpatterns = [
     path('postscreate/', PostListCreateView.as_view(), name='post-list-create'),
+    
+    path(' ', RetrieveView.as_view(), name='post-retrieve-update-destroy'),
     path('postlist/', RetrieveView.as_view(), name='post-retrieve-update-destroy'),
     path('posts/<int:pk>/like/', PostLikeView.as_view(), name='post-like'),
     path('posts/<int:pk>/share/', PostShareView.as_view(), name='post-share'),
